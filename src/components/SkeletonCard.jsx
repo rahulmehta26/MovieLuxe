e1ea05b928af04d7652c11a564a6103e71cce5ad
Rahul Kumar Mehta <rk6658542@gmail.com>
2024-12-14 23:00:24 +0530

    responsive

--- src/components/SkeletonCard.jsx
+++ src/components/SkeletonCard.jsx
@@ -6,9 +6,9 @@ const SkeletonCard = () => {
     <div className="flex space-x-6">
       {Array.from({ length: 5 }).map((_, index) => (
         <div key={index} className="flex-shrink-0">
-          <div className="w-80 space-y-2 flex flex-col justify-end bg-gray-200 p-4 rounded-md h-52 animate-pulse">
-            <div className="w-full bg-gray-300 p-1.5 rounded"></div>
-            <div className="w-[50%] bg-gray-300 p-1.5 rounded"></div>
+          <div className="w-80 space-y-2 flex flex-col justify-end bg-gray-300 p-4 rounded-md h-52 animate-pulse">
+            <div className="w-full bg-gray-400 p-1.5 rounded"></div>
+            <div className="w-[50%] bg-gray-400 p-1.5 rounded"></div>
           </div>
         </div>
       ))}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import {getFirestore} from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXwnx6leQf5sjx6xQceycjmRTGetfLK4U",
-  authDomain: "movieluxe-40cd7.firebaseapp.com",
-  projectId: "movieluxe-40cd7",
-  storageBucket: "movieluxe-40cd7.firebasestorage.app",
-  messagingSenderId: "735770033093",
-  appId: "1:735770033093:web:1c739367946282c155a9ac"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
